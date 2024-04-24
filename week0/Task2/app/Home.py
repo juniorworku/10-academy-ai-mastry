0fc1db63f04ab35204bca9d97fac6fa886119e9d
juniorworku <juniorworku@gmail.com>
2024-04-25 00:38:09 +0300

    dataset added

--- week0/Task2/app/Home.py
+++ week0/Task2/app/Home.py
@@ -8,19 +8,3 @@ import seaborn as sns
 
 # Title
 st.title("MoonLight Energy Solutions Dashboard")
-
-# Sidebar
-st.sidebar.title("Options")
-option = st.sidebar.selectbox("Select Dataset", ["Benin Malanville", "Sierra Leone Bumbuna", "Togo Dapaong QC"])
-
-# Main Content
-if option == "Benin Malanville":
-    st.subheader("Benin Malanville Dataset")
-
-    # Add more visualizations and analysis here
-elif option == "Sierra Leone Bumbuna":
-    st.subheader("Sierra Leone Bumbuna Dataset")
-
-    # Add more visualizations and analysis here
-elif option == "Togo Dapaong QC":
-    st.subheader("Togo Dapaong QC Dataset")
